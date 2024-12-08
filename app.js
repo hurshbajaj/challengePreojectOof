f023f4580ff20ca55bdb124d6c1ad9bb8095574d
Hursh Bajaj <hurshbajaj2011@gmail.com>
2024-12-08 23:07:56 +0530

    #1

---
+++ app.js
@@ -0,0 +1,44 @@
+const http = require('http');
+const fs = require("fs");
+const path = require("path")
+let server = http.createServer((req, res) => {
+    let baseURL = req.url.split("?")[0]
+    let params = req.url.split("?")[1];
+    console.log(params)
+    if(params === undefined || !params){
+        params = "No Params Provided"
+    }
+    if(req.method != "POST"){
+        switch (baseURL) {
+        case "/":
+            res.writeHead(200, { "Content-Type": "text/html" });
+            let stream2 = fs.createReadStream(path.join(__dirname, "/normal.html"))
+            stream2.pipe(res)
+            break;
+        case "/dynamic":
+            res.writeHead(200, { "Content-Type": "text/html" });
+            let stream3 = fs.createReadStream(path.join(__dirname, "/dynamic.html"))
+            stream3.pipe(res)
+            break;
+
+        default:
+            res.writeHead(200, { "Content-Type": "image/jpg" });
+            let pathToImg = path.join(__dirname, "/img.jpg")
+            let stream = fs.createReadStream(pathToImg)
+            stream.pipe(res);
+            break;
+        }
+    }
+    else if(req.method === "POST"){
+        res.writeHead(200, {"content-Type": "text/plain"})
+        res.end(params);
+
+    }
+    
+
+    }
+)
+
+server.listen(3000, 'localhost', ()=>{
+    console.log("Listening");
+})
